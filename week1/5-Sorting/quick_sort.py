45de6a7de3bf9d124bdaa9277b847bd414f1fcc5
brada1 <ivo.neykov@gmail.com>
2015-06-19 22:16:31 +0300

    improved class structure of quick sort

--- week1/5-Sorting/quick_sort.py
+++ week1/5-Sorting/quick_sort.py
@@ -4,49 +4,49 @@ class QuickSort:
    # Sorts a sequence of integers.
    def sort(sequence):
       """perform quick sort"""
-      quickSortHelper(sequence,0,len(sequence)-1)
+      QuickSort.quickSortHelper(sequence,0,len(sequence)-1)
       return (sequence)
 
-def quickSortHelper(alist,first,last):
-   if first<last:
+   def quickSortHelper(alist,first,last):
+      if first<last:
 
-       splitpoint = partition(alist,first,last)
+          splitpoint = QuickSort.partition(alist,first,last)
 
-       quickSortHelper(alist,first,splitpoint-1)
-       quickSortHelper(alist,splitpoint+1,last)
-
-
-def partition(alist,first,last):
-   pivotvalue = alist[first]
-
-   leftmark = first+1
-   rightmark = last
-
-   done = False
-   while not done:
-
-       while leftmark <= rightmark and \
-               alist[leftmark] <= pivotvalue:
-           leftmark = leftmark + 1
-
-       while alist[rightmark] >= pivotvalue and \
-               rightmark >= leftmark:
-           rightmark = rightmark -1
-
-       if rightmark < leftmark:
-           done = True
-       else:
-           temp = alist[leftmark]
-           alist[leftmark] = alist[rightmark]
-           alist[rightmark] = temp
-
-   temp = alist[first]
-   alist[first] = alist[rightmark]
-   alist[rightmark] = temp
+          QuickSort.quickSortHelper(alist,first,splitpoint-1)
+          QuickSort.quickSortHelper(alist,splitpoint+1,last)
 
 
-   return rightmark
+   def partition(alist,first,last):
+      pivotvalue = alist[first]
 
-##seq = [4, 13, 52, 7, 18, 3, 1, 6]
-##print (QuickSort.sort(seq))
+      leftmark = first+1
+      rightmark = last
+
+      done = False
+      while not done:
+
+          while leftmark <= rightmark and \
+                  alist[leftmark] <= pivotvalue:
+              leftmark = leftmark + 1
+
+          while alist[rightmark] >= pivotvalue and \
+                  rightmark >= leftmark:
+              rightmark = rightmark -1
+
+          if rightmark < leftmark:
+              done = True
+          else:
+              temp = alist[leftmark]
+              alist[leftmark] = alist[rightmark]
+              alist[rightmark] = temp
+
+      temp = alist[first]
+      alist[first] = alist[rightmark]
+      alist[rightmark] = temp
+
+
+      return rightmark
+
+seq = [4, 13, 52, 7, 18, 3, 1, 6]
+print (QuickSort.sort(seq))
 
