3a39cb44f7487f4c808cc436eeefed71f90421a8
brada1 <ivo.neykov@gmail.com>
2015-06-25 13:43:55 +0300

    improve solution of bst
    the solution couldn't be really tested before

--- week3/1-BST/bst.py
+++ week3/1-BST/bst.py
@@ -1,34 +1,57 @@
+import sys
+
 class BST:
-    
-    # Checks if a binary tree is a binary search tree.
-    # root - node with 'left', 'right' and 'value' properties
-    
-    inf = float("infinity")
-    ninf = float("-infinity")
-    
-    def isBST(root, minVal = ninf, maxVal = inf):
-        '''based on simple recursion alongside check for validity'''
-        if root is None:
+    '''this class instantiates a tree and calls the isBST check '''
+    def __init__(self):
+        self.root = None
+
+    def isBST(self):
+        '''checks if the binary tree instance is a bst'''
+        self.root.isBST()
+        return result
+
+    def insert(self, data):
+        '''used to build a bst, refers to Node().insert for the most part'''
+        if self.root:
+            return self.root.insert(data)
+        else:
+            self.root = Node(data)
             return True
 
-        if not minVal <= root.value <= maxVal:
+global last, result
+result = True
+last = -sys.maxsize
+class Node:
+    def __init__(self, val):
+        self.value = val
+        self.left = None
+        self.right = None
+
+    def isBST(self):
+        '''performes an in-order traversal and check if the elements are sorted'''
+        global last, result
+        if self:
+            if self.left:
+                self.left.isBST()
+            if self.value <= last:
+                result = False
+            last = self.value
+            if self.right:
+                self.right.isBST()
+                
+    def insert(self, data):
+        '''does the heavy lifting of building a bst'''
+        if self.value == data:
             return False
-
-        return BST.isBST(root.left, minVal, root.value) and \
-               BST.isBST(root.right, root.value, maxVal)
-
-
-
-    def isBST2(root, lastNode = [ninf]):
-        '''check if in-order traversal provides the nodes in proper sorted fashion'''
-        if root is None:
-            return True
-
-        if not BST.isBST2(root.left, lastNode):
-            return False
-
-        if root.value < lastNode[0]:
-            return False
-
-        lastNode[0] = root.value
-        return BST.isBST2(root.right, lastNode)
+        elif self.value > data:
+            if self.left:
+                return self.left.insert(data)
+            else:
+                self.left = Node(data)
+                return True
+        else:
+            if self.right:
+                return self.right.insert(data)
+            else:
+                self.right = Node(data)
+                return True
--- week3/1-BST/bst_test.py
+++ week3/1-BST/bst_test.py
@@ -1,66 +1,14 @@
-import sys, os, time
+# this script checks if bst.py is callable externally and if it works well
+import sys, os, random
 if os.getcwd() not in sys.path:
     sys.path.append(os.getcwd())
 from bst import BST
 
-# first form a bst
-class Node:
-    '''each node of the tree is kept as an instance of the class'''
-    def __init__(self, val):
-        self.value = val
-        self.left = None
-        self.right = None
+bst = BST()
+array = random.sample(range(0, 100), 10)
+print (array)
+for i in range(10):
+    bst.insert(array[i])
+    
+print (bst.isBST())
 
-    def insert(self, data):
-        if self.value == data:
-            return False
-        elif self.value > data:
-            if self.left:
-                return self.left.insert(data)
-            else:
-                self.left = Node(data)
-                return True
-        else:
-            if self.right:
-                return self.right.insert(data)
-            else:
-                self.right = Node(data)
-                return True
-
-    def find(self, data):
-        if self.value == data:
-            return True
-        elif self.value > data:
-            if self.left:
-                return self.keft.find(data)
-            else:
-                return False
-        else:
-            if self.rightChild:
-                return self.right.find(data)
-            else:
-                return False
-
-class Tree:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, data):
-        if self.root:
-            return self.root.insert(data)
-        else:
-            self.root = Node(data)
-            return True
-
-    def find(self, data):
-        if self.root:
-            return self.root.find(data)
-        else:
-            return False
-
-bst = Tree()
-for i in range(500):
-    bst.insert(i)
- 
-print(BST.isBST(Node(0)))
-print(BST.isBST2(Node(0)))
